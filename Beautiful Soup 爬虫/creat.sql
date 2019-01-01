7dd96501ce3a9d04d8264e8ec6417e64152812f4
alphariver <14136330+alphariver@users.noreply.github.com>
2019-01-01 11:10:28 +0800

    Create creat.sql

---
+++ Beautiful Soup 爬虫/creat.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `user` (
+  `btype_id` int(11) NOT NULL AUTO_INCREMENT,
+  `b_type` varchar(10) NOT NULL,
+  `cn_new_type` varchar(20),
+  `cn_old_type` varchar(20),
+  `inner_diameter` double DEFAULT '0',
+  `outer_diameter` double DEFAULT '0',
+  `width` double DEFAULT '0',
+  `weight` double DEFAULT '0',
+  PRIMARY KEY (`btype_id`)
+) ;
