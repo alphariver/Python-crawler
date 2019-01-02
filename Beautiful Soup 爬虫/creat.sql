ba49356d268fde8c4a11f70faedad22f5dc51c8e
alphariver <14136330+alphariver@users.noreply.github.com>
2019-01-02 17:50:16 +0800

    Update creat.sql

--- Beautiful Soup 爬虫/creat.sql
+++ Beautiful Soup 爬虫/creat.sql
@@ -9,3 +9,13 @@ CREATE TABLE `bearingType` (
   `weight` double DEFAULT '0',
   PRIMARY KEY (`btype_id`)
 ) ;
+
+CREATE TABLE `correspond_type` (
+  `cor_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `b_type` varchar(10) NOT NULL,
+  `cn_new_type` varchar(20),
+  `cn_old_type` varchar(20),
+  `foreign_brand` varchar(20),
+  `foreign_type` varchar(20),
+  PRIMARY KEY (`cor_type_id`)
+) ;
